62a2e32ebc8a8f8260ccd93667eb14c1da9264d5
Satendra <satendrachauhan7665234@gmail.com>
2024-06-26 16:42:40 +0530

    First message

---
+++ src/Components/Pages/Player/Player.js
@@ -0,0 +1,9 @@
+import React from 'react'
+import './Player.css'
+
+
+export const Player = () => {
+  return (
+    <div>Player</div>
+  )
+}
---
+++ src/Components/TitleCards/TitleCards.js
@@ -0,0 +1,36 @@
+import React, { useEffect, useRef } from 'react'
+import './TitleCards.css'
+import cards_data from '../../assets/cards/Cards_data'
+
+
+const TitleCards = ({title , category}) => {
+
+  const cardsref = useRef();
+
+const handleWheel = (event)=> {
+  event.preventDefault();
+  cardsref.current.scrollLeft += event.deltaY;
+}
+
+useEffect (() => {
+  cardsref.current.addEventListener('wheel' , handleWheel)
+} , [])
+
+  return (
+    <div className='title-cards'>
+        <h2>{title ? title : "Popular on Netflix"}</h2>
+        <div className='card-list' ref= {cardsref}>
+           {
+            cards_data.map((card , index) =>{
+                return <div className='card' key={index}>
+                   <img src = { card.image } alt='' />
+                   <p>{ card.name }</p>
+                </div>
+            })
+           }
+        </div>
+    </div>
+  )
+}
+
+export default TitleCards
